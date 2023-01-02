a9c09715812d305071c9ad56d72f533d5f8fc062
Samuel Cooper <samuel.j.cooper@outlook.com>
2023-01-02 21:50:07 +0900

    file upload added

--- server/api.py
+++ server/api.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request, redirect
 from flask_restful import Resource, Api
+import os
 from server.models import Image
 from server.models import Character
 
@@ -15,7 +16,17 @@ def init_api(app):
 
     @app.route("/upload-image", methods=["POST"])
     def upload_image():
-        ()
+        if request.method == "POST":
+            if "file" not in request.files:
+                return "No File Submitted\n"
+            id = request.form["id"]
+            file = request.files["file"]
+            if file.filename == "":
+                return "Filename Empty\n"
+            file.save(app.root_path + os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+            return "Success\n"
+        return "Method not supported\n"
+
 
     api = Api(app)
     api.add_resource(ImageResource, "/image/<string:image_id>")
